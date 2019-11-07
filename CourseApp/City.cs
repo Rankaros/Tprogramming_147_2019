7e08ca70617438e707023279c87eb808deedf3de
дмитрий овсяник <mitya.ovsyanik.99@mail.ru>
2019-11-07 10:42:04 +0300

    City v1.0

---
+++ CourseApp/City.cs
@@ -0,0 +1,48 @@
+using System;
+
+namespace CourseApp
+{
+    public class City
+    {
+
+        private int pop;
+
+        public City()
+        : this (0, "Unitiled", "Unnamed")
+        {
+        }
+
+        public City(int pop, string country, string name)
+        {
+            Pop = pop;
+            Country = country;
+            Name = name;
+        }
+        
+        public string Country {get; set;}
+
+        public string Name {get; set;}
+
+        public int Pop
+        {
+            get 
+            {
+                return this.pop;
+            }
+
+            set
+            {
+                if(value >=0)
+                {
+                    this.pop=value;
+                }
+                else
+                {
+                    Console.WriteLine("Population should be > 0.");
+                }
+            }
+        }
+
+        public int Pop {get; set;}
+    }
+}
