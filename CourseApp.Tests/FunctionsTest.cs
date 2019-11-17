078f242c8a19ad5936072aa4b82c008c574d94bf
дмитрий овсяник <mitya.ovsyanik.99@mail.ru>
2019-11-17 15:11:07 +0300

    FunctionsTest v1.0

---
+++ CourseApp.Tests/FunctionsTest.cs
@@ -0,0 +1,36 @@
+using System;
+using Xunit;
+
+namespace CourseApp.Tests
+{
+    public class FunctionsTest
+    {
+        [Theory]
+        [InlineData(0, 0, 0, double.NaN)]
+        [InlineData(2, 2, 2, 2.5)]
+        [InlineData(-2, -2, 2, 2.5)]
+
+        public void TestCalc(double a, double b, double x, double exp)
+        {
+            var res = Program.MyFunction(a, b, x);
+            Assert.Equal(exp, res, 3);
+        }
+
+        [Fact]
+        public void TestNormalA()
+        { //Это тоже надо заполнять?
+        }
+
+        [Fact]
+        public void TestNormalB()
+        { //И это?
+        }
+
+        [Fact]
+        public void TestZeroLengthB()
+        {
+            var res = Program.TaskB(2, 3, new double[0]);
+            Assert.Empty(res);
+        }
+    }
+}
